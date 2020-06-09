b95b2f07a140df7279747bc71f9e616520f1c2df
NK <45469562+navapro@users.noreply.github.com>
2020-06-09 07:19:08 -0600

    created the 2d array
    Created the 2d array using for loop.

--- sketch.js
+++ sketch.js
@@ -1,61 +1,52 @@
 // Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let cols = 25;
+let rows = 25;
+let grid = [];
+let w;
+let h;
+let path = [];
 
-let cannonX ;
-let cannonY ;
-let cannonWidth ;
-let cannonLength ;
-let cannonAngle = 0;
-let bullets = [];
+let openSet = [];
+let closedSet = [];
+
+let startNode;
+let endNode;
+
+
+
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cannonX =75;
- cannonY = windowHeight - 150;
- cannonWidth = 50;
- cannonLength = 125;
-}
 
-function draw() {
-  background(220);
-  displaycannon();
-  updateBullets();
-}
+  // Create a 2D array using for loop.
+  createCanvas(400, 400);
 
-function displaycannon(){
-  push();
-  translate(cannonX, cannonY);
-  cannonAngle = atan2(mouseY - cannonY, mouseX - cannonX);
-  rotate(cannonAngle)
-  rect(0, - cannonWidth/2, cannonLength,cannonWidth,);
-  circle (0,0,cannonWidth);
-  pop();
-}
 
-function mouseClicked(){
-  fire();
+  w = width / cols;
+  h = height / rows;
 
-}
-
-function fire(){
-  let thisBullet = {
-    x: cannonX,
-    y: cannonY,
-    r: cannonWidth,
-    angle: cannonAngle,
-    speed: 5
-  };
-  bullets.push(thisBullet);
-
-}
-function updateBullets(){
-  for(let thisBullet of bullets){
-    thisBullet.x += thisBullet.speed * cos(thisBullet.angle);
-    thisBullet.y += thisBullet.speed* sin(thisBullet.angle);
-    circle(thisBullet.x,thisBullet.y,thisBullet.r);
+  for (let i = 0; i < cols; i++) {
+    grid.push([]);
   }
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Node(i, j);
+    }
+  }
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
+  }
+  startNode = grid[0][0];
+  endNode = grid[cols - 1][rows - 1];
+
+  openSet.push(startNode);
+
+
 }
+
+
+
