63619b31bed322cfee01e54dfa992b248c73c656
NK <45469562+navapro@users.noreply.github.com>
2020-06-27 10:50:48 -0600

    checkbox for diagonal search
    Checkbox for diagonal search can be true for searching diagonally and false for not.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ function heuristic(a, b) {
   return d;
 }
 
+let checkbox;
 let showPath = false;
 let cols = 25;
 let rows = 25;
@@ -90,19 +91,20 @@ function Spot(i, j) {
       this.neighbors.push(grid[i][j - 1]);
     }
 
-    if (i > 0 && j > 0) {
-      this.neighbors.push(grid[i - 1][j - 1]);
+    if (checkbox.value()) {
+      if (i > 0 && j > 0) {
+        this.neighbors.push(grid[i - 1][j - 1]);
+      }
+      if (i < cols - 1 && j > 0) {
+        this.neighbors.push(grid[i + 1][j - 1]);
+      }
+      if (i > 0 && j < rows - 1) {
+        this.neighbors.push(grid[i - 1][j + 1]);
+      }
+      if (i < cols - 1 && j < rows - 1) {
+        this.neighbors.push(grid[i + 1][j + 1]);
+      }
     }
-    if (i < cols - 1 && j > 0) {
-      this.neighbors.push(grid[i + 1][j - 1]);
-    }
-    if (i > 0 && j < rows - 1) {
-      this.neighbors.push(grid[i - 1][j + 1]);
-    }
-    if (i < cols - 1 && j < rows - 1) {
-      this.neighbors.push(grid[i + 1][j + 1]);
-    }
-
 
   }
 
@@ -143,7 +145,8 @@ function setup() {
 
   openSet.push(start);
 
-
+  checkbox = createCheckbox('label', false);
+  checkbox.changed(myCheckedEvent);
 
 
 }
@@ -391,7 +394,6 @@ function showText() {
   fill(0, 102, 153, 51);
   text('word', 10, 90);
 }
-
 
 // diagonal check box
 // slider for speed
