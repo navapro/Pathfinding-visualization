a8d7eec224e4944e1b9f9564b3dfccfc9e9043cf
NK <45469562+navapro@users.noreply.github.com>
2020-07-30 12:24:50 -0600

    Moved backtracking to a new file

---
+++ backtrack.js
@@ -0,0 +1,12 @@
+function backTrack(current){
+        done = true;
+        path = [];
+        var temp = current;
+        path.push(temp);
+        while (temp.previous) {
+          path.push(temp.previous);
+          temp = temp.previous;
+        }
+
+        showPath = true;
+}
